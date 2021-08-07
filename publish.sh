d27da8260b2c51d19493270b4b4477bd1db79252
Fran Simó <fransimo@gmail.com>
2021-08-07 08:51:46 +0200

    publish simplification

---
+++ publish.sh
@@ -0,0 +1,1 @@
+pandoc README.md -s -f markdown -t html -c pandoc.css --metadata title="Fran Simó" --metadata subtitle="“Art is plastic thought. A swing between ideas and aesthetics.”" -o index.html
